ebe0f4aeb2ccf5daf6db9acd9067e17cc6989b2a
ChangeOn <46215283+ChangeOn@users.noreply.github.com>
2020-04-06 20:30:08 +0900

    Add files via upload
    #2 Setting Typescript Up

---
+++ index.ts
@@ -0,0 +1,12 @@
+const name = "Raymond",
+    age = 29,
+    gender = "female";
+    
+const sayHi = (name, age, gender) => {
+    console.log('Hi ${name}, you are ${age}, you are a ${gender}');
+};
+
+sayHi(name, age, gender);
+
+
+export {};
